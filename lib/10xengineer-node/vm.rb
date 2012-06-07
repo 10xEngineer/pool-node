90a48a29a8833b8a75bc743d80a354d8ae9a79d0
Radim Marek <radim@laststation.net>
2012-06-07 19:18:28 +0200

    VM model

---
+++ lib/10xengineer-node/vm.rb
@@ -0,0 +1,44 @@
+require 'yajl'
+require 'time'
+
+module TenxEngineer
+  module Node
+    class VM
+      attr_accessor :id, :state, :pool, :type, :options, :created_at, :updated_at
+
+      def initialize(id, state, pool, type, options = {}, created_at = Time.now, updated_at = Time.now)
+        @id = id
+        @state = state
+        @pool = pool
+        @type = type
+        @options = options
+        @created_at = created_at
+        @updated_at = updated_at
+      end
+
+      def touch
+        @updated_at = Time.now
+      end
+
+      def self.from_json(json)
+        h = Yajl::Parser.parse(json)
+
+        VM.new(h["id"], h["state"].to_sym, h["pool"], h["type"], h["options"], Time.parse(h["created_at"]), Time.parse(h["updated_at"]))
+      end
+
+      def to_json
+        hash = {
+          :id => @id,
+          :state => @state,
+          :pool => @pool,
+          :type => @type,
+          :options => @options,
+          :created_at => @created_at.iso8601,
+          :updated_at => @updated_at.iso8601
+        }
+
+        Yajl::Encoder.encode(hash)
+      end
+    end
+  end
+end
