47c00a8c39e0156b964673f19c4137f7000d2f6d
Radim Marek <radim@laststation.net>
2012-06-14 09:16:50 +0200

    Load VM from file

--- lib/10xengineer-node/vm.rb
+++ lib/10xengineer-node/vm.rb
@@ -4,7 +4,7 @@ require 'time'
 module TenxEngineer
   module Node
     class VM
-      attr_accessor :id, :state, :pool, :type, :options, :created_at, :updated_at
+      attr_accessor :id, :state, :pool, :type, :options, :created_at, :updated_at, :ip_address
 
       def initialize(id, state, pool, type, options = {}, created_at = Time.now, updated_at = Time.now)
         @id = id
@@ -12,6 +12,7 @@ module TenxEngineer
         @pool = pool
         @type = type
         @options = options
+        @ip_address = nil
         @created_at = created_at
         @updated_at = updated_at
       end
@@ -21,15 +22,23 @@ module TenxEngineer
       end
 
       def save!
-        vm_file = "#{TenxEngineer::Node::ROOT}/data_bags/vms/#{id}.json"
+        vm_file = VM.vm_file(id)
 
-        open(vm_file, "w") { |f| f << vm.to_json }
+        open(vm_file, "w") { |f| f << self.to_json }
+      end
+
+      def self.load(id)
+        VM.from_json(File.read(VM.vm_file(id)))
       end
 
       def self.from_json(json)
         h = Yajl::Parser.parse(json)
 
         VM.new(h["id"], h["state"].to_sym, h["pool"], h["type"], h["options"], Time.parse(h["created_at"]), Time.parse(h["updated_at"]))
+      end
+
+      def self.vm_file(id)
+        "#{TenxEngineer::Node::ROOT}/data_bags/vms/#{id}.json"
       end
 
       def to_json
