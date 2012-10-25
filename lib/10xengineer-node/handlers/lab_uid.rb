d2457c941861a9e43ecfd9d890c18c2de16ddbba
Radim Marek <radim@laststation.net>
2012-10-25 16:52:29 +0200

    Grant passwordless sudo to lab user

--- lib/10xengineer-node/handlers/lab_uid.rb
+++ lib/10xengineer-node/handlers/lab_uid.rb
@@ -2,6 +2,13 @@
 
 TenxEngineer::External.execute("chroot #{@rootfs} useradd --create-home -s /bin/bash --uid 1000 lab")
 TenxEngineer::External.execute("echo \"lab:lab\" | chroot #{@rootfs} chpasswd")
+TenxEngineer::External.execute("echo \"lab:lab\" | chroot #{@rootfs} adduser lab sudo")
+
+sudoers_t = File.join(File.dirname(__FILE__), '../templates/sudoers.erb')
+erb = Erubis::Eruby.new(File.read(sudoers_t))
+
+suders_f = File.join(@rootfs, "/etc/sudoers")
+File.open(suders_f, 'w') {|f| f.write(erb.result(binding()))}
 
 if @data[:keys]
 	TenxEngineer::External.execute("chroot #{@rootfs} mkdir /home/lab/.ssh")
